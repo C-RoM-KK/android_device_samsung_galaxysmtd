5f222080d81364df938090db32a6d5142fb94f52
aways <aways@c-rom.org>
2014-06-11 03:20:08 +0200

    C-RoMize

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,6 @@ BOARD_SECOND_CAMERA_DEVICE := /dev/video2
 
 #Inline Kernel Config
 TARGET_KERNEL_CONFIG := semaphore_galaxys_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-13.04/bin/arm-linux-gnueabihf-
 
 # FM Radio
 BOARD_HAVE_FM_RADIO := true
